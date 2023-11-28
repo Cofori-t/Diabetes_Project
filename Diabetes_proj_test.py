628424a204ae5892c7ede87eb036dc4aceb8f43e
Charles Ofori <28985131+Cofori-t@users.noreply.github.com>
2023-11-28 14:21:44 +0100

    Update Diabetes_proj_test.py

--- Diabetes_proj_test.py
+++ Diabetes_proj_test.py
@@ -12,12 +12,12 @@ Test
 import pickle
 import pandas as pd
 
-age = st.slider('Age[years]:Range 1-100', 1, 100)
+age = st.slider('Age[years]: Range 1-100', 1, 100)
 hypertension = st.number_input('Hypertension: No=0  Yes=1',0, 1)
 heart_disease = st.number_input('Heart_disease: No=0 Yes=1',0, 1)
-bmi = st.slider('BMI[kg/m**2]::Range 16.0-80.0',16.0, 80.0)
-HbA1c_level = st.slider('HbA1c_Level[%]:Range 4.0-10.0',4.0,10.0)
-blood_glucose_level = st.slider('Blood_Glucose_Level[mg/dL]: range 60-250', 60.0,250.0)
+bmi = st.slider('BMI[kg/m**2]: Range 16.0-80.0',16.0, 80.0)
+HbA1c_level = st.slider('HbA1c_Level[%]: Range 4.0-10.0',4.0,10.0)
+blood_glucose_level = st.slider('Blood_Glucose_Level[mg/dL]: Range 60-250', 60.0,250.0)
 gender_num  = st.number_input('Gender_num: male=0 female=1 other=2',0,2)
 smoking_history_num  = st.number_input('Smoking_History_num: No Info = -1, never = 0, former=1, current = 2,not current = 3, ever = 4',-1,4)
 new_input = pd.DataFrame({
