7fa7202173df5f31fc673ba7b3c0e111312ad7e1
Charles Ofori <28985131+Cofori-t@users.noreply.github.com>
2023-12-04 18:01:06 +0100

    Update Diabetes-Project_Final.py

--- Diabetes-Project_Final.py
+++ Diabetes-Project_Final.py
@@ -1,14 +1,15 @@
+
 import streamlit as st
 import sklearn
 import pickle
 import pandas as pd
+import numpy as np
 
-#st.set_page_config(layout="wide",
-                  #page_title="Prediction: Diabetes using Logistic Model",
-    #page_icon="📽️")  # this needs to be the first Streamlit command
 
 st.sidebar.title("Control Panel")
-st.write("---")
+
+st.sidebar.subheader(" " ,divider='rainbow' )
+
 st.title("Am I a potential Diabetic Patient?")
 
 st.write("""
@@ -30,28 +31,17 @@ Diabetes. (Categorical outcome)
 Machine Learning Algorithm using Logistic Regression Model.
 
 """)
-#st.markdown("*Check out the [article](https://www.crosstab.io/articles/staged-rollout-analysis) for a detailed walk-through!*")
 with st.container():
-    # add a divider
-    st.sidebar.write("---")
-    #add header for the input part
+   
     st.sidebar.subheader("1. Complete: questionnaire. 2. Click on: Submit Answers")
     st.sidebar.write("##")
     
-#st.sidebar.subheader("Distribution of diabetic")
-
-#import pickle
-#import pandas as pd
-
-#st.markdown("# Main page 🎈")
-#st.sidebar.markdown("# Main page 🎈")
-
-age = st.sidebar.number_input('What is your age[Years]?: Range 1 - 100',min_value=1, max_value=100, step=1)
-hypertension = st.sidebar.number_input('Do you have a high blood pressure (Hypertension))? - Choose your answer: No=0  Yes=1', min_value=0, max_value=1, step=1)
-heart_disease = st.sidebar.number_input('Do you have Heart_disease? - Choose your answer: No=0  Yes=1)',min_value=0, max_value=1, step=1)
-bmi = st.sidebar.number_input('What is your BMI[kg/m**2] - Enter your value: Range 10.0 - 96.0)',min_value=10.0, max_value=96.0, step=.1)
-HbA1c_level = st.sidebar.number_input('What is your haemoglobin value (HbA1c_Level[%]) - Enter your value: Range 3.0 - 10.0)',min_value=3.0, max_value=10.0, step=.1)
-blood_glucose_level = st.sidebar.number_input('Your Glucose Level (Blood_Glucose_Level[mg/dL]) - Enter your value: range 60 - 250)', min_value=60, max_value=300, step=1)
+age = st.sidebar.slider('What is your age[Years]?: Range 1 - 80',1, 80, 3, 1)
+hypertension = st.sidebar.number_input('Do you have a high blood pressure (Hypertension))? - Choose your answer: No=0  Yes=1', min_value=0, max_value=1)
+heart_disease = st.sidebar.number_input('Do you have Heart_disease? - Choose your answer: No=0  Yes=1)',min_value=0, max_value=1)
+bmi = st.sidebar.slider('What is your BMI[kg/m**2] - Enter your value: Range 10.0 - 96.0)',10.00, 96.00, 18.5,0.5)
+HbA1c_level = st.sidebar.slider('What is your haemoglobin value (HbA1c_Level[%]) - Enter your value: Range 3.0 - 9.0)', 3.5, 9.0, 4.0, 0.1 )
+blood_glucose_level = st.sidebar.slider('Your Glucose Level (Blood_Glucose_Level[mg/dL]) - Enter your value: range 60 - 300)', 80, 300, 10, 5)
 gender_num  = st.sidebar.number_input('What is your Gender (Gender_num) -  Choose your answer: male=0  female=1  other=2)',min_value=0, max_value=2, step=1)
 smoking_history_num  = st.sidebar.number_input('Your Smoking Habits (Smoking_History_num) - Choose your answer: No Info = -1  never = 0  former=1  current = 2  not_current = 3  ever = 4)',
                                                min_value=-1, max_value=4, step=1)
@@ -68,53 +58,55 @@ with st.container():
       
 })
 
-#pipe=pickle.load(open('WBS/ML/pipe.pkl1','rb'))
-#load model and create new dataframe
+
 with st.container():
-    #pipe=pickle.load(open('WBS/ML/pipe_pkl1','rb'))
-    pipe=pickle.load(open('pipe_pkl1','rb'))
-
-# create a botton for prediction
-
-#if st.sidebar.button('Submit Answers'):
+    pipe=pickle.load(open('WBS/ML/pipe_pkl1','rb'))
     
     prediction = pipe.predict(new_input)
     
+st.snow()
 
-#st.markdown("# Results ")
-st.markdown("# Results ")
-if st.sidebar.button('Submit Answers'):
+st.markdown("## Results ")
+
+st.subheader(" " ,divider='rainbow' )
+
+st.sidebar.subheader(" " ,divider='rainbow' )
+
+if st.sidebar.button('Submit Answers',type="primary"):
   if (prediction[0]==1):
-    st.write("# Yes!!!! likely of been Diabetic")
+    st.write("# :red[Yes!!!! likely of been Diabetic]")
   else:
-    st.write("# No!! I am not Diabetic 🎉")
+    st.write("# :green[No!! I am not Diabetic] 🎉")
     
+st.subheader(" " ,divider='rainbow' ) 
 
 # 7 add like for help
 with st.container():
-    #add a divider
-    st.write("---")
+    
     st.title("Conclusion")
     st.write("""
-            This model only detect, if there is a likelihood of being diabetic OR Not and not the Type.
-            
-            For further diagnosis, PLEASE consult your physician, who will confirm on which type, and remedies to take.
-            
-            There are different of Diabetes: TYPES: 1, 2, Pre-diabates, Gestational Diabetes. 
-            
-            Symptoms: thirsty, freq. Urination, Weight Loss, fatigue and weakness
-            """)
-    st.subheader("""[More Information about Diabetes]([https://www.worldpopulationreview.com/country-rankings/diabetes-rates-by-country])""")
+             This model only detect, if there is a likelihood of being diabetic OR Not. 
+             
+             There are different of Types of Diabetes namely:
+             
+             TYPES: 1, 2, Pre-diabates, Gestational Diabetes.
+             
+             For further diagnosis, PLEASE consult your physician, who will confirm on which type, and remedies to take.   
+             
+             """)
+    
+    st.subheader("References" ,divider='rainbow') 
+        
+    st.subheader("""[More Information about Diabetes](https://worldpopulationreview.com/country-rankings/diabetes-rates-by-country) 
+                   
+                """)
+    
+    st.subheader("""[More Information about Ranges](https://www.forbes.com/health/body/normal-blood-sugar-levels/)""")
+    
+
     st.subheader("""[Data source](https://www.kaggle.com/code/jayrdixit/diabetes-prediction/input?select=diabetes_prediction_dataset.csv)""")
     
     st.write("##")
-    st.write("---")
-    
-    
-#from PIL import Image
+    st.subheader(" " ,divider='rainbow' )
 
-#Load an image from a local file (e.g., 'sunrise.jpg')
-#image = Image.open('sunrise.jpg')
-
-#Display the image with an optional caption
-#st.image(image, caption='Sunrise by the mountains')
+    
