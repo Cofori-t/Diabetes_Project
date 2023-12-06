b2a91ce139610d4f3b5a67c0b2f8235ec60aba90
Charles Ofori <28985131+Cofori-t@users.noreply.github.com>
2023-12-06 15:41:02 +0100

    Update Diabetes-Project-Final.py

--- Diabetes-Project-Final.py
+++ Diabetes-Project-Final.py
@@ -19,15 +19,17 @@ Diabetes is a disease that occurs when the blood glucose, also called blood suga
 
 According to the International Diabetes Federation (IDF) data, 537 million people live with Diabetes, which may increase to 643 million by 2030 and to 783 million by 2045.
 
-As my project work, I have created a Model that could be used to predict the likelihood of Diabetes in patients based on their Medical history and Demographic details.:
+My project work: 
+
+I created a Model that could be used to predict the likelihood of Diabetes in patients based on their Medical history and Demographic details.
 
 Dependent Var: 
 
-Gender, Age, Hypertension(Blood-Pressure), Heart Disease, Body Mass Index (BMI), Smoking history, HbA1c level, and Blood Glucose Level
+:blue[Gender, Age, Hypertension(Blood-Pressure), Heart Disease, Body Mass Index (BMI), Smoking history, HbA1c level, and Blood Glucose Level]
 
 Target Var:
 
-Diabetes. (Binary outcome)
+:blue[Diabetes. (Binary outcome)]
 
 Machine Learning Algorithm using Logistic Regression Model.
 
